e41a84c9b0a587ca23598f51e3c7ad09355b879f
Luca Tabone <taboneluca3@gmail.com>
2016-11-22 21:57:38 +0100

    Return expected object in hooks

--- src/hooks/messages/input.js
+++ src/hooks/messages/input.js
@@ -4,5 +4,11 @@ module.exports = (worker) => {
   return (context, record, updated) => {
     const eventName = `api:${context.request.type}:${context.request.method}`
     worker.emit(eventName, record, updated)
+
+    switch (context.request.method) {
+      case 'create': return record
+      case 'update': return updated
+      case 'delete': return null
+    }
   }
 }
