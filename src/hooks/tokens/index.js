4532ba3c9bdb26655dd094af8bffd76f5aced02c
Luca Tabone <taboneluca3@gmail.com>
2017-02-06 19:53:27 +0100

    Reinclude the token value
    Token value is needed by users who have the GRANT_TOKEN to create
    the cookie.

--- src/hooks/tokens/index.js
+++
@@ -1,5 +0,0 @@
-'use strict'
-
-module.exports = (worker) => {
-  return [null, require('./output')(worker)]
-}
--- src/hooks/tokens/output.js
+++
@@ -1,8 +0,0 @@
-'use strict'
-
-module.exports = () => {
-  return (context, record) => {
-    delete record.token
-    return record
-  }
-}
