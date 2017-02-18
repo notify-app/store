c106667220649dab6f3ee82f9b9743701f4de869
Luca Tabone <taboneluca3@gmail.com>
2017-02-18 14:00:10 +0100

    Provide 'unread' info when requesting records by ID
    Currently when a consumer requested a list users (without
    specifying) IDs, the API returned the list of unread messages of
    each resource. In order to optimize the payload this was changed
    and now this info is provided when the consumer requests for user
    resources by ID.

--- src/hooks/users/output.js
+++ src/hooks/users/output.js
@@ -5,6 +5,8 @@ module.exports = (worker) => {
     const eventName = `api:${context.request.type}:${context.request.method}`
     worker.emit(eventName, record, context)
 
+    // Unread info should only be provided when requesting records by id.
+    if (context.request.ids === null) delete record.unread
     // Internal ID should not be accessible by the client app.
     delete record.internalID
     // Author info of a message should be retrieved from the messages endpoint.
